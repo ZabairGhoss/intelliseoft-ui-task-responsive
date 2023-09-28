5e79763d2521583f5ffd9ec02ef42c2e484323e3
Zabair Ghoss <czubair307@gmail.com>
2023-09-28 22:13:02 +0500

    Started working on reponsiveness

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    new Splide('#splide', {
-        type: 'grid',
-        grid: {
-            rows: 2,    // Number of rows
-            cols: 3,    // Number of columns
-            gap: '', // Spacing between cards
+//   document.addEventListener('DOMContentLoaded', function () {
+//     new Splide('#splide', {
+//         type: 'grid',
+//         grid: {
+//             rows: 2,    // Number of rows
+//             cols: 3,    // Number of columns
+//             gap: '', // Spacing between cards
             
-        },
-        pagination: false, // Remove pagination (optional)
-        arrows: true,     // Show navigation arrows (optional)
-    }).mount();
-});
+//         },
+//         pagination: false, // Remove pagination (optional)
+//         arrows: true,     // Show navigation arrows (optional)
+//     }).mount();
+// });
 
-});
+// });
