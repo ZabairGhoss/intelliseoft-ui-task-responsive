2e7e7fd4e165935e2b177fc2d681c7a35a7866ba
Zabair Ghoss <czubair307@gmail.com>
2023-09-28 21:32:08 +0500

    Splide carosel slider embbeded

--- js/slider.js
+++ js/slider.js
@@ -1,59 +1,18 @@
-// $(document).ready(function() {
-//     $('.slider').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//     });
-// });
-
 $(document).ready(function() {
-// $('.custom-slider').slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 5,
-//     slidesToScroll: 1,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   }
-    //   // You can unslick at a given breakpoint now by adding:
-    //   // settings: "unslick"
-    //   // instead of a settings object
-    // ]
-  // });
 
-  // $('.custom-slider').slick({
-  //   infinite: true,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 3
-  // });
 
-  $('.custom-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+  document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#splide', {
+        type: 'grid',
+        grid: {
+            rows: 2,    // Number of rows
+            cols: 3,    // Number of columns
+            gap: '', // Spacing between cards
+            
+        },
+        pagination: false, // Remove pagination (optional)
+        arrows: true,     // Show navigation arrows (optional)
+    }).mount();
+});
 
 });
