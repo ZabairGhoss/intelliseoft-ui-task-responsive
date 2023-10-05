117f998c959efdd613c6f72b44c2feee2fdec7b0
Zabair Ghoss <czubair307@gmail.com>
2023-10-05 17:46:16 +0500

    Fixing UI Bugs for the final app

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,30 @@
-// $(document).ready(function() {
-
-
-//   document.addEventListener('DOMContentLoaded', function () {
-//     new Splide('#splide', {
-//         type: 'grid',
-//         grid: {
-//             rows: 2,    // Number of rows
-//             cols: 3,    // Number of columns
-//             gap: '', // Spacing between cards
-            
+// $(document).ready(function () {
+//   document.addEventListener("DOMContentLoaded", function () {
+//     new Splide("#splide", {
+//       type: "grid",
+//       grid: {
+//         rows: 2, // Number of rows
+//         cols: 3, // Number of columns
+//         gap: "", // Spacing between cards
+//       },
+//       pagination: false, // Remove pagination (optional)
+//       arrows: false, // Show navigation arrows (optional)
+//       pagination: true,
+//       paginationDirection: "ltr",
+//       breakpoints: {
+//         800: {
+//           grid: {
+//             rows: 2,
+//             cols: 1,
+//           },
 //         },
-//         pagination: false, // Remove pagination (optional)
-//         arrows: true,     // Show navigation arrows (optional)
+//         600: {
+//           grid: {
+//             rows: 4,
+//             cols: 1,
+//           },
+//         },
+//       },
 //     }).mount();
-// });
-
+//   });
 // });
